59ec0e0d387c06629b4f91177088f01e2b6d7d2d
Liam Lynch <liam.lynch@dataiku.com>
2024-01-18 11:29:17 +0100

    improved some variable naming and deleted some dead code [sc-161126]

--- python-lib/attachment_handling.py
+++ python-lib/attachment_handling.py
@@ -22,33 +22,30 @@ def attachments_template_dict(attachment_datasets):
     return attachments_dict
 
 
-def build_attachments(attachments, attachment_type):
+def build_attachments(attachment_datasets, attachment_type):
     """
-    :param attachments: List of attachment datasets
+    :param attachment_datasets: List of attachment datasets
     :param attachment_type: str, e.g. "excel", "csv"
     :return: Attachments as List of MIMEApplication
     """
 
+    if attachment_type == "excel":
+        request_fmt = "excel"
+    else:
+        request_fmt = "tsv-excel-header"
+
     # Prepare attachments
     mime_parts = []
-    for a in attachments:
-
-        if attachment_type == "excel":
-            request_fmt = "excel"
-        else:
-            request_fmt = "tsv-excel-header"
-            filename = a.full_name + ".csv"
-            mimetype = ["text", "csv"]
-
-        with a.raw_formatted_data(format=request_fmt) as stream:
+    for attachment_ds in attachment_datasets:
+        with attachment_ds.raw_formatted_data(format=request_fmt) as stream:
             buf = stream.read()
 
         if attachment_type == "excel":
             app = MIMEApplication(buf, _subtype="vnd.openxmlformats-officedocument.spreadsheetml.sheet")
-            app.add_header("Content-Disposition", 'attachment', filename=a.full_name + ".xlsx")
+            app.add_header("Content-Disposition", 'attachment', filename=attachment_ds.full_name + ".xlsx")
             mime_parts.append(app)
         else:
             txt = MIMEText(buf, _subtype="csv", _charset="utf-8")
-            txt.add_header("Content-Disposition", 'attachment', filename=a.full_name + ".csv")
+            txt.add_header("Content-Disposition", 'attachment', filename=attachment_ds.full_name + ".csv")
             mime_parts.append(txt)
     return mime_parts
