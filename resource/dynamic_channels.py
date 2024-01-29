3719bfce64fc703832f5bbe70433143ecfc30dd0
Liam Lynch <liam.lynch@dataiku.com>
2024-01-29 18:23:32 +0100

    removing 'Channel:' from select names

--- resource/dynamic_channels.py
+++ resource/dynamic_channels.py
@@ -15,9 +15,9 @@ def do(payload, config, plugin_config, inputs):
             if 'smtp' in channel.get('type') or 'mail' in channel.get('type'):
                 if channel.get('sender'):
                     # If the channel has a sender append `(<sender email>)` to label and `_S` flag to channel ID
-                    choices.append(f"Channel: {channel.get('id')} ({channel.get('sender')})", channel.get('id') + SENDER_SUFFIX)
+                    choices.append(f"{channel.get('id')} ({channel.get('sender')})", channel.get('id') + SENDER_SUFFIX)
                 else:
-                    choices.append(f"Channel: {channel.get('id')}",  channel.get('id'))
+                    choices.append(f"{channel.get('id')}",  channel.get('id'))
 
         # Add an entry for direct SMTP
         if len(channels) > 0:
